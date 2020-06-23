8627fe475e38b1b53d765e654dbce44a614dd476
joserobjr <joserobjr@gamemods.com.br>
2020-06-23 17:04:14 -0300

    Create Align GitHub Headers.user.js

---
+++ Align GitHub Headers.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Align GitHub Headers
+// @namespace    http://github.com/GameModsBr
+// @version      0.3
+// @description  I hate that the GitHub headers are misaligned in the new layout, so I made this to fix that
+// @author       joserobjr
+// @match        https://github.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    let addGlobalStyle = function(css) {
+        var head, style;
+        head = document.getElementsByTagName('head')[0];
+        if (!head) { return; }
+        style = document.createElement('style');
+        style.type = 'text/css';
+        style.innerHTML = css;
+        head.appendChild(style);
+    }
+    setTimeout(function(){
+        addGlobalStyle('div.pagehead > * { max-width: 1280px; margin-left: auto; margin-right: auto; }');
+        addGlobalStyle('div.pagehead { border-bottom: 1px solid #e1e4e8 !important; }');
+        addGlobalStyle('div.pagehead > nav.UnderlineNav { box-shadow: none !important; overflow: visible !important; }');
+        addGlobalStyle('div.pagehead > nav > div.position-absolute.pr-lg-5 { position: inherit !important; padding: 0 !important; }');
+        addGlobalStyle('div.pagehead > nav > div.position-absolute.pr-lg-5 > details > summary > div { padding-right: 0 !important; }');
+        addGlobalStyle('div.pagehead > nav > div.position-absolute.pr-lg-5 > details > details-menu { margin-right: -10px; }');
+    }, 5);
+})();
